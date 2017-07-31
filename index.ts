7d30e0d2beb4dc03351ffb7f0e7c9ff4a0af52fd
Chris <chris@xappmedia.com>
2017-07-31 13:25:02 -0400

    Adding proxy

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import * as express from "express";
 
+const proxy = require("http-proxy-middleware");
+
 const PORT = 8080;
-const HOST = "0.0.0.0";
+const HOST = "127.0.0.1";
+
+const REDIRECT_DOMAIN: string = process.env["DOMAIN"] || "https://api.xapp.media";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world\n");
+console.log("DOMAIN = " + REDIRECT_DOMAIN);
+
+const routeProxy = proxy({
+    target: REDIRECT_DOMAIN,
+    changeOrigin: true,
+    topLevel: "debug"
 });
+
+app.get("/*", routeProxy);
 
 app.listen(PORT, HOST);
 console.log("Running on http://" + HOST + ":" + PORT);
